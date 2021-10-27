d43e57574c86f7dc7f36a275eb3050e54f17ca9f
Pavel Kuzminov <minecraftpavel@gmail.com>
2021-10-27 16:48:47 +0200

    Add project files.

---
+++ MonitorOSD/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonitorOSD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //http://sysman.sll.se/SysMan/api/monitoring?target=LITLS13395850
+        static string NetResponseToString(string uriString)
+        {
+            Uri uri = new Uri(uriString);
+            WebRequest webRequest = WebRequest.Create(uri);
+
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream receiveStream = webResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(receiveStream, encode);
+            Char[] read = new Char[256];
+
+            int count = readStream.Read(read, 0, 256);
+            string output = "";
+            while (count > 0)
+            {
+                String Str = new String(read, 0, count);
+                output += Str;
+                count = readStream.Read(read, 0, 256);
+            }
+            readStream.Close();
+            webResponse.Close();
+            return output;
+        }
+    }
+}
---
+++ MonitorOSD/MonitoringLookup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorOSD
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class MonitoringLookup
+    {
+        public string id { get; set; }
+        public string target { get; set; }
+        public string hardwareModelName { get; set; }
+        public int status { get; set; }
+        public int steps { get; set; }
+        public int currentStep { get; set; }
+        public string currentStepName { get; set; }
+        public object vmHost { get; set; }
+        public object vmName { get; set; }
+        public string dartIp { get; set; }
+        public int dartPort { get; set; }
+        public string dartTicket { get; set; }
+        public object passphrase { get; set; }
+        public object endDate { get; set; }
+        public object createdDate { get; set; }
+        public object lastReportedDate { get; set; }
+        public string createdBy { get; set; }
+        public object lastModifiedDate { get; set; }
+        public string lastModifiedBy { get; set; }
+        public int warnings { get; set; }
+        public int errors { get; set; }
+        public double progress { get; set; }
+        public bool isUnresponsive { get; set; }
+    }
+}
